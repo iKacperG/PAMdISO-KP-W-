6e78b1bd1fee346e268409e95885f2a9b5c5c4d2
iKacperG <lumisworkshop@gmail.com>
2026-05-09 08:22:46 +0200

    Zad2: add expenses log from fixed array of data

---
+++ zadanie-02.js
@@ -0,0 +1,17 @@
+const expenses = [18.5, 42, 9.99, 27, 61.3, 15, 33.5];
+
+const totalSpent = expenses.reduce((sum, expense) => sum + expense, 0);
+const averageExpense = totalSpent / expenses.length;
+const highestExpense = Math.max(...expenses);
+const minorExpensesThreshold = 20;
+const minorExpenses = expenses.filter((expense) => expense < minorExpensesThreshold).length;
+
+function formatMoney(value) {
+  return `${value.toFixed(2)} zł`;
+}
+
+console.log("Raport budżetu tygodniowego");
+console.log(`Suma wydatków: ${formatMoney(totalSpent)}`);
+console.log(`Średni wydatek: ${formatMoney(averageExpense)}`);
+console.log(`Największy wydatek: ${formatMoney(highestExpense)}`);
+console.log(`Liczba wydatków poniżej ${formatMoney(minorExpensesThreshold)}: ${minorExpenses}`);
